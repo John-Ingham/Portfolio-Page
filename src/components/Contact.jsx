27a1c62b145adf33fb81603099723964d6df231e
John-Ingham <johnaingham@yahoo.co.uk>
2021-12-06 16:37:57 +0000

    webform work

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,6 +17,7 @@ const Contact = () => {
     e.preventDefault()
     fetch('/', {
       method: 'POST',
+
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
       body: encode({ 'form-name': 'contact', name, email, message }),
     })
@@ -64,7 +65,7 @@ const Contact = () => {
           </div>
         </div>
         <form
-          netlify
+          data-netlify="true"
           name="contact"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
